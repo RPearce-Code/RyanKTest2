8a4bf82b384ed0b3eceb77b8e6fa58f56cc501c0
kress <kress@10.0.0.246>
2025-03-28 19:52:07 -0400

    Initial Setup

---
+++ src/test2/CustomerOrderTest.java
@@ -0,0 +1,14 @@
+package test2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerOrderTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
